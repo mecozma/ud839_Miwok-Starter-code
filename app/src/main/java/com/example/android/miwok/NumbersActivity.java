4697433b3d170aa572ffd2bc11edd2dd4f10b4ac
mecozma <mecozma@gmail.com>
2018-05-31 14:46:54 +0100

    Added TextViews for all the ArrayList elements using a for loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,16 +15,25 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String[] numbers = new String[10];
-        numbers[0] = "one";
-        numbers[1] = "two";
-        numbers[2] = "three";
-        numbers[3] = "four";
-        numbers[4] = "five";
-        numbers[5] = "six";
-        numbers[6] = "seven";
-        numbers[7] = "eight";
-        numbers[8] = "nine";
-        numbers[9] = "ten";
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+
+        for (int index = 0; index < words.size(); index++) {
+
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+        }
     }
 }
