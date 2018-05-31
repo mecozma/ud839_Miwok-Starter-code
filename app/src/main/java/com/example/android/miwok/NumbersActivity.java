04556b50d04e3e184593bee96c72fc57af32eb5c
mecozma <mecozma@gmail.com>
2018-05-31 16:35:42 +0100

    I added an ArrayAdapter in the NumberActivity.java file and ListView in activity_numbers.hml file to populate the view with content

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -27,13 +29,10 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
-        for (int index = 0; index < words.size(); index++) {
+        ListView listView = (ListView) findViewById(R.id.list);
 
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(index));
-            rootView.addView(wordView);
-        }
+        listView.setAdapter(itemsAdapter);
     }
 }
